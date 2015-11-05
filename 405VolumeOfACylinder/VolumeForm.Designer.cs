d906650c5de54c7dd5de2b71783f8ce3ca99edca
pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-11-05 14:15:08 -0500

    Done

---
+++ 405VolumeOfACylinder/VolumeForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace _405VolumeOfACylinder
+{
+    partial class frmVolume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudRadius = new System.Windows.Forms.NumericUpDown();
+            this.nudHeight = new System.Windows.Forms.NumericUpDown();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRadius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nudRadius
+            // 
+            this.nudRadius.Location = new System.Drawing.Point(12, 54);
+            this.nudRadius.Name = "nudRadius";
+            this.nudRadius.Size = new System.Drawing.Size(120, 20);
+            this.nudRadius.TabIndex = 0;
+            // 
+            // nudHeight
+            // 
+            this.nudHeight.Location = new System.Drawing.Point(152, 54);
+            this.nudHeight.Name = "nudHeight";
+            this.nudHeight.Size = new System.Drawing.Size(120, 20);
+            this.nudHeight.TabIndex = 1;
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(9, 38);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(40, 13);
+            this.lblRadius.TabIndex = 2;
+            this.lblRadius.Text = "Radius";
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Enabled = false;
+            this.txtVolume.Location = new System.Drawing.Point(92, 154);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(100, 20);
+            this.txtVolume.TabIndex = 3;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 4;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(92, 135);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(103, 13);
+            this.lblVolume.TabIndex = 5;
+            this.lblVolume.Text = "Volume of a Cylinder";
+            this.lblVolume.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(152, 38);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 6;
+            this.lblHeight.Text = "Height";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(110, 93);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmVolume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 201);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.nudHeight);
+            this.Controls.Add(this.nudRadius);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmVolume";
+            this.Text = "Volume of a Cylinder";
+            ((System.ComponentModel.ISupportInitialize)(this.nudRadius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudRadius;
+        private System.Windows.Forms.NumericUpDown nudHeight;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ 405VolumeOfACylinder/VolumeForm.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by: Nicholas Ellul
+ * Created on: 05-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 4-05
+ * This program calculates the volume of a cylinder.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _405VolumeOfACylinder
+{
+    public partial class frmVolume : Form
+    {
+
+        public double VolumeOfCylinder(double theRadius, double theHeight)
+        {
+            // This calculates the volume of a cylinder
+            double theVolume;
+
+            theVolume = Math.PI * Math.Pow(theRadius,2) * theHeight;
+            theVolume = Math.Round(theVolume, 2);
+
+            return theVolume;
+        }
+
+        public frmVolume()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //variables
+
+            double height;
+            double radius;
+            double volume;
+            string volumeAsString;
+
+            radius = Convert.ToDouble(this.nudRadius.Value);
+            height = Convert.ToDouble(this.nudHeight.Value);
+
+            //process
+
+            volume = VolumeOfCylinder(radius, height);
+
+            //output
+
+            volumeAsString = Convert.ToString(volume);
+            this.txtVolume.Text = volumeAsString;
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
